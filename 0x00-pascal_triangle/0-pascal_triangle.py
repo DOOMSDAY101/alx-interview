e7461c91fa35088b973276859c2aa47286c2654f
ifeoluwa sulaiman <ifeoluwasulaiman30@gmail.com>
2023-06-29 06:06:47 +0000

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """
-Pascal's Triangle
+0-pascal_triangle
 """
 
 
-def binomialCoeff(n, k):
-    """ calculate the value of Binomial Coefficient """
-    res = 1
-    if (k > n - k):
-        k = n - k
-    for i in range(0, k):
-        res = res * (n - i)
-        res = res // (i + 1)
-    return res
-
 def pascal_triangle(n):
     """
-    Returns list of lists of integers representing
-    the Pascal’s triangle of n or an empty list if n <= 0
-    You can assume n will be always an integer
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
-    l1 = []
-    for line in range(0, n):
-        l2 = []
-        for i in range(0, line + 1):
-            l2.append(binomialCoeff(line, i))
-        l1.append(l2)
-    return l1
+    k = []
+    if n <= 0:
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k

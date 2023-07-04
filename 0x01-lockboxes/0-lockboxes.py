5043d233d2229ec723c0629ea10c42075dfaace5
ifeoluwa sulaiman <ifeoluwasulaiman30@gmail.com>
2023-07-04 16:04:08 +0000

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,13 +7,14 @@ def canUnlockAll(boxes):
     based on keys that can be attained.
     Solution to the lockboxes problem
     """
-    if not boxes or type(boxes) is not list:
-        return False
-    unlocked = [0]
-    for n in unlocked:
-        for key in boxes[n]:
-            if key not in unlocked and key < len(boxes):
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)

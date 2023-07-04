7cecf582600347902debe6cdcbaa767f6d74ea63
ifeoluwa sulaiman <ifeoluwasulaiman30@gmail.com>
2023-07-04 15:26:34 +0000

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """ LockBoxes Function """
+    T = []
+    for i in range(1, len(boxes)):
+        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
+        if i in T:
+            T = []
+        else:
+            return False
+    return True

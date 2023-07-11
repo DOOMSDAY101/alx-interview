25bd26f9c86b2f7e20709ec94ee5297229af5340
ifeoluwa sulaiman <ifeoluwasulaiman30@gmail.com>
2023-07-11 16:01:51 +0000

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Find minimum number of operations to create string with COPY & PASTE"""
 
-from math import sqrt
+""" Module that calculates operations """
 
 def minOperations(n):
-    """Sums all the factors of n to find the 'minimum operations'"""
-    sumFactors = 0
-    if n <= 1:
+    """
+    Method that calculates the fewest number of
+    operations needed
+    """
+    if n < 2 or type(n) is not int:
         return 0
-    for i in range(2, int(sqrt(n) + 1)):
-        while n % i == 0: # i is a factor
-            sumFactors += i
-            n /= i
-            if n <= 1:
-                break
-    if n > 1:
-        sumFactors += int(n)
-    return sumFactors
+    check = []
+    val = n
+    i = 1
+    while val != 1:
+        i += 1
+        if val % i == 0:
+            while (val % i == 0 and val != 1):
+                val = val / i
+                check.append(i)
+    return sum(check)

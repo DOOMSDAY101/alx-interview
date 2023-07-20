2bbda8f688a39a8f4a14ec67ff3e92f5cefd50c4
ifeoluwa sulaiman <ifeoluwasulaiman30@gmail.com>
2023-07-20 17:28:10 +0000

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
+
     def print_stats(stats: dict, file_size: int) -> None:
         print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
